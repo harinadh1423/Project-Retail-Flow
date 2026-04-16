761d3ba59d97d7181edbddb500559d7ad0b6c41f
harinadh1405 <2483500@cognizant.com>
2026-04-16 17:29:45 +0530

    init commit

---
+++ retailproject/src/main/java/com/project/retailproject/controller/userController.java
@@ -0,0 +1,37 @@
+package com.project.retailproject.controller;
+
+import com.project.retailproject.dto.UserDTO;
+import com.project.retailproject.dto.UserResponseDTO;
+import com.project.retailproject.model.User;
+import com.project.retailproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class userController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/insertUser")
+    public ResponseEntity<UserResponseDTO> insertUser(UserDTO userDTO) {
+        User user =  userService.insertUser(userDTO.getUser());
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setUser(user);
+        userResponseDTO.setMessage("success");
+        userResponseDTO.setStatusCode(200);
+        return ResponseEntity.ok().body(userResponseDTO);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<UserResponseDTO> updateUser(UserDTO userDTO) {
+        User user =  userService.updateUser(userDTO.getUser());
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setUser(user);
+        userResponseDTO.setMessage("success");
+        userResponseDTO.setStatusCode(200);
+        return ResponseEntity.ok().body(userResponseDTO);
+    }
+}
--- retailproject/src/main/java/com/project/retailproject/service/UserService.java
+++ retailproject/src/main/java/com/project/retailproject/service/UserService.java
@@ -1,4 +1,46 @@
 package com.project.retailproject.service;
 
+
+
+import com.project.retailproject.db.UserRepository;
+import com.project.retailproject.dto.UserDTO;
+import com.project.retailproject.dto.UserResponseDTO;
+import com.project.retailproject.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User insertUser(User user) {
+       return userRepository.save(user);
+
+    }
+
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    public User getUser(int id) {
+        return userRepository.findById(id).get();
+    }
+
+
+
 }
