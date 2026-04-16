27dd0c3925e2166f532d05a6028f23a72847bf31
ViswaTeja <AvulaViswaTeja.Reddy@cognizant.com>
2026-04-16 17:16:20 +0530

    created UserService file

--- retailproject/src/main/java/com/project/retailproject/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.retailproject.repository;
-
-import com.project.retailproject.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
