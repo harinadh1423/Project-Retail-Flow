d072d41421b9bf77f74ec77359b585c61a8341ef
harinadh1405 <2483500@cognizant.com>
2026-04-16 17:31:46 +0530

    File change

--- retailproject/src/main/java/com/project/retailproject/controller/UserController.java
+++ retailproject/src/main/java/com/project/retailproject/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
-public class userController {
+public class UserController {
 
 
     @Autowired
